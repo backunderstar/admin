f8fc61eb9e8860502a2d07f15aa1ca3ae286b6f7
admin <admin@example.com>
2026-06-22 21:12:41 +0800

    feat: add ZhaoIconButton reusable icon button component

---
+++ src/components/layouts/zhao-icon-button/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    class="flex items-center justify-center w-8 h-8 rounded-md cursor-pointer text-(--color-text-3) hover:text-(--color-text-1) hover:bg-(--color-fill-2) transition-colors"
+    @click="$emit('click')"
+  >
+    <ZhaoIcon :icon="icon" class="text-lg" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import ZhaoIcon from '@/components/icons/ZhaoIcon.vue'
+
+defineOptions({ name: 'ZhaoIconButton' })
+
+defineProps<{
+  icon: string
+}>()
+
+defineEmits<{
+  click: []
+}>()
+</script>
